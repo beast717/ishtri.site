1e2fda62abe73b408d2ac14e6555dc5bfe73d275
beast717 <ahmedalshaikh2002@gmail.com>
2025-04-14 21:44:06 +0200

    bugs

--- instrument.js
+++ instrument.js
@@ -1,5 +1,6 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
 const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
   dsn: "https://4cb627e4e2cb38629abf1676342ede0f@o4509152984498176.ingest.de.sentry.io/4509152986726480",
